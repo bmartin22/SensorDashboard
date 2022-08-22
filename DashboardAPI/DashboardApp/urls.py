ee0759f69feaa10c9aaf47056a54485fe4ae1ec1
IamusBrian <brian.martin@iamus.ai>
2022-08-22 18:01:22 +0100

    Adding serialization for API

---
+++ DashboardAPI/DashboardApp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import re_path
+from DashboardApp import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns=[
+    re_path(r'^user/$',views.userApi),
+    re_path(r'^user/([0-9]+)$',views.userApi),
+
+    re_path(r'^sensor/$',views.sensorApi),
+    re_path(r'^sensor/([0-9]+)$',views.sensorApi),
+
+    re_path(r'^reading/$',views.readingApi),
+    re_path(r'^reading/([0-9]+)$',views.readingApi),
+
+    #re_path(r'^savefile/',views.saveFile)
+]#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- DashboardUI/UserInterface/src/app/shared.service.ts
+++ DashboardUI/UserInterface/src/app/shared.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class SharedService {
 
-  constructor() { }
+export class SharedService {
+  readonly APIUrl = "http://127.0.0.1:8000";
+
+  constructor(private http:HttpClient) { }
+  
+  //---------USER SERVICES---------//
+  //return list of users
+  getUserList():Observable<any[]>{
+    return this.http.get<any[]>(this.APIUrl + '/user/');
+  }
+
+  //update user 
+  updateUserList(val:any){
+    return this.http.put(this.APIUrl + '/user/', val);
+  }
+  
+  //---------SENSOR SERVICES---------//
+  //return list of sensors
+  getSensorList():Observable<any[]>{
+    return this.http.get<any[]>(this.APIUrl + '/sensor/');
+  }
+
+  //update sensor
+  updateSensorList(val:any){
+    return this.http.put(this.APIUrl + '/sensor/', val)
+  }
+
+  //delete sensor
+  deleteSensorList(val:any){
+    return this.http.delete(this.APIUrl + '/sensor/'+val);
+  }
+
+  //add a new sensor
+  addSensor(val:any){
+    return this.http.post(this.APIUrl + '/sensor/', val);
+  }
+
+  //---------READING SERVICES---------//
+  //return list of readings
+  getReadingList():Observable<any[]>{
+    return this.http.get<any[]>(this.APIUrl + '/reading/');
+  }
+
+  //delete reading
+  deleteReading(val:any){
+    return this.http.delete(this.APIUrl + '/delete/'+val);
+  }
+
+  //
 }
